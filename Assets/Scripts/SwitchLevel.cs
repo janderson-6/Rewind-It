ae6386b75935d2b8f8ec13f73339a98deb30e1a9
MohamadAlsamman <moealsamman@gmail.com>
2023-03-22 12:18:54 +0000

    Added Retry buttons i the player loses and a quit game button in the main menu

--- Assets/Scripts/SwitchLevel.cs
+++ Assets/Scripts/SwitchLevel.cs
@@ -34,32 +34,42 @@ public class SwitchLevel : MonoBehaviour
 
         else if (sceneName == "Level 1")
         {
-            Debug.Log("load 2");
             SceneManager.LoadScene("Level 2");
         }
 
         else if (sceneName == "Level 2")
         {
-            Debug.Log("load 3");
             SceneManager.LoadScene("Level 3");
         }
 
         else if (sceneName == "Level 3")
         {
-            Debug.Log("load 4");
             SceneManager.LoadScene("Level 4");
         }
 
         else if (sceneName == "Level 4")
         {
-            Debug.Log("load 5");
             SceneManager.LoadScene("Level 5");
         }
 
         else if (sceneName == "Level 5")
         {
-            Debug.Log("load 6");
             SceneManager.LoadScene("Level 6");
         }
+
+        else if (sceneName == "Level 6")
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+
+    public void RetryLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
